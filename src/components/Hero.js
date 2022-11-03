fd9312595985d4f3fa6477d9b8c2c640649169f3
Rahul Puri <rahulxpuri@gmail.com>
2022-11-03 23:01:17 +0530

    setup the data

---
+++ src/components/Hero.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+function Hero({heroapi}) {
+  return (
+    <>
+    <div>
+    <div></div>
+    <div>
+        <div>
+            <h1></h1>
+            <h1></h1>
+        <button></button>
+        <div></div>
+        <div></div>
+        </div>
+        <div>
+        <img className='' src="" alt="hero-img/img" />
+        </div>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
